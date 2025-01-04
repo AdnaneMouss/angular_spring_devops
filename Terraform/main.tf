4a5a2a2977e3778289f2afe6af71425500fec675
spapowW <uirandonnee@gmail.com>
2025-01-04 05:44:46 +0100

    issam's commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,39 +1,66 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "docker" {}
+
+# Définir un réseau Docker pour les conteneurs
 resource "docker_network" "app_network" {
   name = "app_network"
 }
-resource "docker_container" "angular_app" {
-  name  = "angular_app"
+
+# Créer un conteneur pour l'application Angular
+resource "docker_container" "angular" {
+  name  = "angular"
   image = "node:14"
   ports {
     internal = 4200
     external = 4200
   }
   volumes {
-    host_path      = "./angular-app"
+    host_path      = "${path.cwd}/angular"
     container_path = "/usr/src/app"
   }
   networks_advanced {
     name = docker_network.app_network.name
   }
-
-  command = "npm start"
+  command = ["sh", "-c", "npm install && npm start"]  # Installer d'abord les dépendances, puis démarrer Angular
+  depends_on = [docker_container.mysql_db]  # Assurer que MySQL est lancé avant Angular
 }
+
+# Variable pour le mot de passe root de MySQL
+variable "mysql_root_password" {
+  description = "Le mot de passe root de MySQL"
+  type        = string
+  sensitive   = true
+}
+
+# Créer un conteneur pour MySQL
 resource "docker_container" "mysql_db" {
-  name  = "mysql_db"
-  image = "mysql:5.7"
-  env {
-    MYSQL_DATABASE = "inventory"
-  }
+  name  = "mysql_dbb"
+  image = "mysql:8.0"
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${var.mysql_root_password}",
+    "MYSQL_DATABASE=inventory"
+  ]
+
   ports {
     internal = 3306
-    external = 3306
+    external = 3306  # Assurez-vous que ce port est disponible sur votre machine
   }
+
   networks_advanced {
     name = docker_network.app_network.name
   }
+
   volumes {
-    host_path      = "./mysql-data"
+    host_path      = "${path.cwd}/mysql-data"
     container_path = "/var/lib/mysql"
   }
 }
