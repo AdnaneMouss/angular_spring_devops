1a6620353d0c5558e1bb3327fd01fab4147a5b65
AmiraBelbsir <belbsiramira@gmail.com>
2025-01-14 00:30:01 +0100

    Amira

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-
+import { LoggingService } from './services/logging.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,12 +9,17 @@ import { Router, NavigationEnd } from '@angular/router';
 export class AppComponent {
   isLoginPage = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private loggingService: LoggingService) {
     // Listen to route changes and determine if the current route is 'login'
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.isLoginPage = this.router.url === '/login'; // Adjust if your login route is different
       }
     });
+  }
+  ngOnInit(): void {
+    // Envoyer des logs
+    this.loggingService.log('Application Angular démarrée', 'info');
+    this.loggingService.log('Ceci est un avertissement', 'warn');
   }
 }
